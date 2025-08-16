502976f250ffac6f5789265ce0d9e796b3c5e692
chunix64 <linoxolus@gmail.com>
2025-08-16 08:49:40 +0000

    Fix zsh_setup.sh order

--- zsh/zsh_setup.sh
+++ zsh/zsh_setup.sh
@@ -3,12 +3,11 @@
 # Install Zsh and Oh My Zsh
 apt install zsh -y
 
+bash <(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)
+
 # Clone new theme and plugins
-git clone https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 git clone https://github.com/zsh-users/zsh-autosuggestions.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 
 rm ~/.zshrc
 curl -fsSL "https://github.com/chunix64/scripts/raw/refs/heads/main/zsh/.zshrc" -o ~/.zshrc
-
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
